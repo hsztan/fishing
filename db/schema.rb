4677ee1448049a1fb76cf802effc0323f8a8c594
Henry Nawrocki <henry@sztanski.com>
2024-09-02 18:55:52 -0500

    chore: Break out of frame with turbo_frame : "_top"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_11_03_190454) do
+ActiveRecord::Schema[7.1].define(version: 2021_11_03_190454) do
   create_table "baits", force: :cascade do |t|
     t.string "name"
     t.string "category"
