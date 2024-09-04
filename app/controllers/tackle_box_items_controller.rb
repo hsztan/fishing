3a98a7086af4c042ac55c65e36c8ce6f494db6d5
Henry Nawrocki <henry@sztanski.com>
2024-09-04 16:49:11 -0500

    turbo streams deep dive

--- app/controllers/tackle_box_items_controller.rb
+++ app/controllers/tackle_box_items_controller.rb
@@ -27,9 +27,12 @@ class TackleBoxItemsController < ApplicationController
     @bait = Bait.find(params[:bait_id])
     @item = current_user.tackle_box_items.create!(bait: @bait)
 
-    # redirect_to baits_url
-
     @bait.my_tackle_box_item = @item
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to baits_url }
+    end
   end
 
   def destroy
@@ -40,7 +43,10 @@ class TackleBoxItemsController < ApplicationController
 
     @bait = @item.bait
     
-    render :create
+    respond_to do |format|
+      format.turbo_stream { render :create }
+      format.html { redirect_to baits_url }
+    end
   end
 
 end
